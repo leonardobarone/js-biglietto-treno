0828b884009f97617422f2bf5e9b6982892a0ac0
leonardobarone <leonardobarone26@gmail.com>
2021-07-29 16:37:40 +0200

    correzzione alessandro

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ var anniPasseggero = prompt('Quanti anni ha il passeggero?');
 var prezzo = chilometriDaPercorrere * 0.21;
 // sconto del 20% per i <= 18
 if (anniPasseggero <= 18) {
-    prezzo = prezzo * 0.2
+    prezzo = prezzo - prezzo * 0.2
 } 
 // sconto del 40% per gli >= 65.
 else if (anniPasseggero >= 65) {
-    prezzo = prezzo * 0.4
+    prezzo = prezzo - prezzo * 0.4
 }
 // Mostra risultato con solo due decimali
 alert("Il prezzo del suo biglietto è di: " + prezzo.toFixed(2) + "€");
